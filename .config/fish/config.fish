24cd7471bd96beb277b80a92cde8213835648605
grizzolo <rizzolo.giovanni98@gmail.com>
2025-11-03 16:02:13 +0100

    remove fish_variables and fish_plugins files, update .gitignore, add Linux file cofig for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -32,6 +32,7 @@ switch (uname)
     case Darwin
         source (dirname (status --current-filename))/config-osx.fish
     case Linux
+	source (dirname (status --current-filename))/config-linux.fish
         fish_add_path /usr/local/bin
         fish_add_path ~/.local/bin
 end
