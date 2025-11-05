ac2976822c2b866a602af7f2663077a7de5b80b2
grizzolo <rizzolo.giovanni98@gmail.com>
2025-11-05 17:15:31 +0100

    fix typo in fish_user_key_bindings, update .gitignore

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -9,7 +9,7 @@ function fish_user_key_bindings
         bind \cr peco_select_history # Bind for peco select history to Ctrl+R
     else
         bind \cf fzf_change_directory # Bind for fzf change directory to Ctrl+F
-        bind \cr _fzf_select_history # Bind for fzf select history to Ctrl+R
+        bind \cr _fzf_search_history # Bind for fzf select history to Ctrl+R
     end
 
     # vim-like
